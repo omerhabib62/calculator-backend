344a168bc1bee0a8e8e07d2dcb4f90e125ad347e
omerhabib62 <omer.habib62@gmail.com>
2025-07-17 07:34:59 +0500

    style: remove unnecessary blank lines in gateway main.ts for cleaner code

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -4,10 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   // Create the HTTP application (gateway)
   const app = await NestFactory.create(AppModule);
-  
+
   // Enable CORS for frontend integration
   app.enableCors();
-  
+
   // Start the HTTP server
   await app.listen(3001);
   console.log('Calculator Gateway is running on http://localhost:3001');
